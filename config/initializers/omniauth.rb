bb628d511a562d802c2a392017d10c52fc217518
Dexter Chua <dalcde@yahoo.com.hk>
2020-05-21 22:36:01 +0800

    Attempt to use raven

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -25,41 +25,42 @@ OmniAuth.config.logger = Rails.logger
 
 # Setup the Omniauth middleware.
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.configuration.omniauth_bn_launcher
-    provider :bn_launcher, client_id: ENV['CLIENT_ID'],
-      client_secret: ENV['CLIENT_SECRET'],
-      client_options: { site: ENV['BN_LAUNCHER_URI'] || ENV['BN_LAUNCHER_REDIRECT_URI'] },
-      setup: SETUP_PROC
-  else
-    Rails.application.config.providers << :ldap if Rails.configuration.omniauth_ldap
-
-    if Rails.configuration.omniauth_twitter
-      Rails.application.config.providers << :twitter
-
-      provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
-    end
-    if Rails.configuration.omniauth_google
-      Rails.application.config.providers << :google
-
-      redirect = ENV['OAUTH2_REDIRECT'].present? ? File.join(ENV['OAUTH2_REDIRECT'], "auth", "google", "callback") : nil
-
-      provider :google_oauth2, ENV['GOOGLE_OAUTH2_ID'], ENV['GOOGLE_OAUTH2_SECRET'],
-        scope: %w(profile email),
-        access_type: 'online',
-        name: 'google',
-        redirect_uri: redirect,
-        setup: SETUP_PROC
-    end
-    if Rails.configuration.omniauth_office365
-      Rails.application.config.providers << :office365
-
-      redirect = ENV['OAUTH2_REDIRECT'].present? ? File.join(ENV['OAUTH2_REDIRECT'], "auth", "office365", "callback") : nil
-
-      provider :office365, ENV['OFFICE365_KEY'], ENV['OFFICE365_SECRET'],
-      redirect_uri: redirect,
-      setup: SETUP_PROC
-    end
-  end
+   provider :ucamraven, ENV['KEY_ID'], ENV['KEY_PATH']
+#  if Rails.configuration.omniauth_bn_launcher
+#    provider :bn_launcher, client_id: ENV['CLIENT_ID'],
+#      client_secret: ENV['CLIENT_SECRET'],
+#      client_options: { site: ENV['BN_LAUNCHER_URI'] || ENV['BN_LAUNCHER_REDIRECT_URI'] },
+#      setup: SETUP_PROC
+#  else
+#    Rails.application.config.providers << :ldap if Rails.configuration.omniauth_ldap
+#
+#    if Rails.configuration.omniauth_twitter
+#      Rails.application.config.providers << :twitter
+#
+#      provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
+#    end
+#    if Rails.configuration.omniauth_google
+#      Rails.application.config.providers << :google
+#
+#      redirect = ENV['OAUTH2_REDIRECT'].present? ? File.join(ENV['OAUTH2_REDIRECT'], "auth", "google", "callback") : nil
+#
+#      provider :google_oauth2, ENV['GOOGLE_OAUTH2_ID'], ENV['GOOGLE_OAUTH2_SECRET'],
+#        scope: %w(profile email),
+#        access_type: 'online',
+#        name: 'google',
+#        redirect_uri: redirect,
+#        setup: SETUP_PROC
+#    end
+#    if Rails.configuration.omniauth_office365
+#      Rails.application.config.providers << :office365
+#
+#      redirect = ENV['OAUTH2_REDIRECT'].present? ? File.join(ENV['OAUTH2_REDIRECT'], "auth", "office365", "callback") : nil
+#
+#      provider :office365, ENV['OFFICE365_KEY'], ENV['OFFICE365_SECRET'],
+#      redirect_uri: redirect,
+#      setup: SETUP_PROC
+#    end
+#  end
 end
 
 # Redirect back to login in development mode.
