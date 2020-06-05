dc74ad44a4abe8d7ce2eef4565b00364accaee7f
Charlie Jonas <charlie@charliejonas.co.uk>
2020-06-05 19:51:12 +0100

    Add icon to copy URL of shared file to clipboard

---
+++ app/assets/javascripts/file_share.js
@@ -0,0 +1,30 @@
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = 0;
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    document.execCommand('copy');
+  } catch (err) {
+    console.error('Failed to copy');
+  }
+  document.body.removeChild(textArea);
+}
+
+$(document).on('turbolinks:load', function() {
+  $('.btn-file-share').click(function(e) {
+    var path = e.target.dataset['fileShare'];
+    copyTextToClipboard(window.location.origin + path);
+  });
+})
