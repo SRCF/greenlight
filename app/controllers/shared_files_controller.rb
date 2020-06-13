dc9ef58b46433d4596f9438f969ceafaba814287
Charlie Jonas <charlie@charliejonas.co.uk>
2020-06-13 02:05:16 +0100

    Use inline Content-Disposition and _blank link targets

--- app/controllers/shared_files_controller.rb
+++ app/controllers/shared_files_controller.rb
@@ -1,6 +1,6 @@
 class SharedFilesController < ApplicationController
   def retrieve
     blob = ActiveStorage::Blob.find_by(key: params[:key])
-    send_data(blob.download, filename: blob.filename.to_s)
+    send_data(blob.download, filename: blob.filename.to_s, disposition: 'inline')
   end
 end
